a9264283fca7ff48f5cbfdb6b7e6b4b31245b964
Jungwoo Kim <kjungw1025@naver.com>
2024-02-13 23:13:54 +0900

    test: 쿠폰 발급 기한에 대한 테스트 코드 작성

--- coupon/coupon-core/src/test/java/com/project/couponcore/model/CouponTest.java
+++ coupon/coupon-core/src/test/java/com/project/couponcore/model/CouponTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CouponTest {
@@ -54,5 +56,53 @@ class CouponTest {
 
         // then
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("발급 기간이 시작되지 않았다면 false를 반환한다.")
+    void availableIssueDate_1() {
+        // given
+        Coupon coupon = Coupon.builder()
+                .dateIssueStart(LocalDateTime.now().plusDays(1))
+                .dateIssueEnd(LocalDateTime.now().plusDays(2))
+                .build();
+
+        // when
+        boolean result = coupon.availableIssueQuantity();
+
+        // then
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("발급 기간에 해당되면 true를 반환한다.")
+    void availableIssueDate_2() {
+        // given
+        Coupon coupon = Coupon.builder()
+                .dateIssueStart(LocalDateTime.now().minusDays(1))
+                .dateIssueEnd(LocalDateTime.now().plusDays(2))
+                .build();
+
+        // when
+        boolean result = coupon.availableIssueDate();
+
+        // then
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("발급 기간이 종료되면 false를 반환한다.")
+    void availableIssueDate_3() {
+        // given
+        Coupon coupon = Coupon.builder()
+                .dateIssueStart(LocalDateTime.now().minusDays(2))
+                .dateIssueEnd(LocalDateTime.now().minusDays(1))
+                .build();
+
+        // when
+        boolean result = coupon.availableIssueDate();
+
+        // then
+        Assertions.assertFalse(result);
     }
 }
