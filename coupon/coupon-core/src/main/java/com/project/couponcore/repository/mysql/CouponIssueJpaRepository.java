f50cbf39af1a4fb91d735a65ba6fa1c29280c32b
Jungwoo Kim <kjungw1025@naver.com>
2024-02-15 10:25:04 +0900

    feat: Coupon, CouponIssue에 대한 JPA 리포지토리 생성

---
+++ coupon/coupon-core/src/main/java/com/project/couponcore/repository/mysql/CouponIssueJpaRepository.java
@@ -0,0 +1,7 @@
+package com.project.couponcore.repository.mysql;
+
+import com.project.couponcore.model.CouponIssue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponIssueJpaRepository extends JpaRepository<CouponIssue, Long> {
+}
---
+++ coupon/coupon-core/src/main/java/com/project/couponcore/repository/mysql/CouponJpaRepository.java
@@ -0,0 +1,7 @@
+package com.project.couponcore.repository.mysql;
+
+import com.project.couponcore.model.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
+}
