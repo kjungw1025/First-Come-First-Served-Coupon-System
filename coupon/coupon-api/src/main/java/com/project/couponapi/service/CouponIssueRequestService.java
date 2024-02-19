a1ae6a39abb9d27f1ae27656d85ac5276737cb64
Jungwoo Kim <kjungw1025@naver.com>
2024-02-19 11:36:35 +0900

    feat: redisson을 통한 분산 락 구현 및 적용

--- coupon/coupon-api/src/main/java/com/project/couponapi/service/CouponIssueRequestService.java
+++ coupon/coupon-api/src/main/java/com/project/couponapi/service/CouponIssueRequestService.java
@@ -1,6 +1,7 @@
 package com.project.couponapi.service;
 
 import com.project.couponapi.controller.dto.CouponIssueRequestDto;
+import com.project.couponcore.component.DistributeLockExecutor;
 import com.project.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +12,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class CouponIssueRequestService {
     private final CouponIssueService couponIssueService;
+    private final DistributeLockExecutor distributeLockExecutor;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        couponIssueService.issue(requestDto.couponId(), requestDto.userId());
+        distributeLockExecutor.execute("lock_" + requestDto.couponId(), 10000, 10000, () -> {
+            couponIssueService.issue(requestDto.couponId(), requestDto.userId());
+        });
         log.info("쿠폰 발급 완료. couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
     }
 }
