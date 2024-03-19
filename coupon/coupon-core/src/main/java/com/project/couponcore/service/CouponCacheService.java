cc27425067680eb0eb899365d8240e7fa2973b3d
kjungw1025 <kjungw1025@naver.com>
2024-03-19 23:33:00 +0900

    feat: 쿠폰 캐시 서비스 로직 추가

---
+++ coupon/coupon-core/src/main/java/com/project/couponcore/service/CouponCacheService.java
@@ -0,0 +1,20 @@
+package com.project.couponcore.service;
+
+import com.project.couponcore.model.Coupon;
+import com.project.couponcore.model.CouponRedisEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CouponCacheService {
+
+    private final CouponIssueService couponIssueService;
+
+    @Cacheable(cacheNames = "coupon")
+    public CouponRedisEntity getCouponCache(long couponId) {
+        Coupon coupon = couponIssueService.findCoupon(couponId);
+        return new CouponRedisEntity(coupon);
+    }
+}
